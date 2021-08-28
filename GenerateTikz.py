7adde294183c5490864e7858cb901446f6fa6d76
LourensT <touwenlourens@gmail.com>
2021-08-28 17:50:33 +0200

    documentation and restructure

--- GenerateTikz.py
+++ GenerateTikz.py
@@ -1,23 +1,25 @@
 import atexit
 from typing import Dict, List
 
+# import the local style dictionary from style.py
 from style import style
-
 
 class GenerateTikz:
 
     counter = 0
 
     def __init__(self, fp, include_colors = False):
-        assert '.tikz' in fp, "should be .tikz file"
-
+        # set the style that was imported
         self.style = style
 
+        # open .tikz file
+        assert '.tikz' in fp, "should be .tikz file"
         self.f = open(fp, 'w')
         
         # make sure file is closed
-        atexit.register(self.f.close)
+        atexit.register(self.finishFile)
 
+        # write first constants
         self.f.write(self.style["start_lines"])
 
         if include_colors:
@@ -28,7 +30,7 @@ class GenerateTikz:
                                 xlog: bool, ylog: bool, 
                                 args: List[str] = []):
 
-        # write constant part 
+        # write constant part of configuration
         self.f.write(self.style["constant_configuration"])
 
         # write axis rules
@@ -49,7 +51,12 @@ class GenerateTikz:
         # close config
         self.f.write("]\n")
 
+    '''
+    Add a series (in dictionary form) to be plotted, by default scatter. Name is used for label. 
+    '''
     def addSeries(self, series : Dict, name : str, scatter=True):
+
+        # select scatter/line styling from the style guide
         if scatter:
             self.f.write("\n\\addplot " + self.style["scatter_styling"][self.counter])
             self.counter += 1
@@ -57,14 +64,24 @@ class GenerateTikz:
             self.f.write("\n\\addplot " + self.style["line_styling"][self.counter])
             self.counter += 1
 
+        # write series data table
         self.f.write("\ntable[row sep=crcr]{\n")
         for key, value in series.items():
             self.f.write(str(key) + "\t" + str(value) + "\\\\ \n" )
         self.f.write("};\n")
+
+        # add legend entry
         self.f.write("\\addlegendentry{" + name + "}\n")
     
+    '''
+    on exit, the file is finished and closed
+    '''
     def finishFile(self):
         self.f.write(self.style["end_lines"])
+
+        self.f.close()
+
+
 
 
 if __name__ == "__main__":
@@ -72,4 +89,3 @@ if __name__ == "__main__":
     #args=["grid = major"]
     cfg.setConfiguration(1980, 2014, 0, 10000, False, True)
     cfg.addSeries({1980 : 900, 2014 : 9000}, "hello")
-    cfg.finishFile()
