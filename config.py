7eea3d178ec4733924ad307e32edf46167015ac3
LourensT <touwenlourens@gmail.com>
2021-08-28 17:32:39 +0200

    working config wrapper

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
 import atexit
+from typing import Dict, List
 
 
 class TikzPlot:
+
+    counter = 0
 
     style = {
         "scatter_styling" :
@@ -24,8 +27,7 @@ class TikzPlot:
         ],
 
         "start_lines" : 
-'''
-\\tikzstyle{every node}=[font=\\normalsize]
+'''\\tikzstyle{every node}=[font=\\normalsize]
 
 \\begin{tikzpicture}
     \\begin{axis}[%
@@ -33,8 +35,7 @@ class TikzPlot:
         ,
 
         "constant_configuration":
-'''
-width=0.951\\fwidth,
+'''width=0.951\\fwidth,
 height=\\fheight,
 at={(0\\fwidth,0\\fheight)},
 scale only axis,
@@ -42,34 +43,30 @@ xminorticks=true,
 yminorticks=true,
 axis x line*=bottom,
 axis y line*=left,
-grid = major,
-x tick label style = {/pgf/number format/.cd,%
-    scaled x ticks = false,
-    set thousands separator={},
-    fixed},
-axis background/.style={fill=white},		axis background/.style={fill=white},
-legend style={legend pos=north west, legend cell align=left, align=left, draw=white!15!black},
-
-'''
+x tick label style = {/pgf/number format/.cd, scaled x ticks = false, set thousands separator={}, fixed},
+axis background/.style={fill=white},
+legend style={legend pos=north west, legend cell align=left, align=left, draw=white!15!black}'''
         ,
         
         "custom_configuration" :
             {
-                "xmin" : 1980,
-                "xmax" : 2014,
-                "xtick" : {1980,1985,1990,...,2015, 2014},
-                "ymode" : "log",
-                "ymin" : 1000,
-                "ymax" : 100000,
-                "ytick" :  {1000.0, 10000.0,  100000.0},
-                "ylabel": "{Yearly Publications}",
+                "xmin" : False,
+                "xmax" : False,
+                #"xtick" : {1980,1985,1990,...,2015, 2014},
+                #"xmode" : "log",
+                #"ymode" : "log",
+                
+                "ymin" : False,
+                "ymax" : False,
+                #"ytick" :  {1000.0, 10000.0,  100000.0},
+                #"ylabel": "{Yearly Publications}",
             }
         ,
 
         "end_lines" : 
 '''
-    \\end{axis}
-\\end{tikzpicture}%
+	\end{axis}
+\end{tikzpicture}
 '''
     }
 
@@ -83,5 +80,48 @@ legend style={legend pos=north west, legend cell align=left, align=left, draw=wh
 
         self.f.write(self.style["start_lines"])
 
+    def setConfiguration(self, xmin, xmax, ymin, ymax, xlog: bool, ylog: bool, args: List[str] = []):
+        self.style["custom_configuration"]["xmin"] = xmin
+        self.style["custom_configuration"]["xmax"] = xmax
+
+        self.style["custom_configuration"]["ymin"] = ymin
+        self.style["custom_configuration"]["ymax"] = ymax
+
+        if xlog:
+            self.style["custom_configuration"]["xmode"] = "log"
+        if ylog:
+            self.style["custom_configuration"]["ymode"] = "log"
+        
+        self.f.write(self.style["constant_configuration"])
+
+        for key, value in self.style["custom_configuration"].items():
+           self.f.write(", \n" + key + "=" + str(value))
+
+        for item in args:
+            self.f.write(", \n" + item)
+
+        self.f.write("]\n")
+
+    def addSeries(self, series : Dict, name : str, scatter=True):
+        if scatter:
+            self.f.write("\n\\addplot " + self.style["scatter_styling"][self.counter])
+            self.counter += 1
+        else:
+            ValueError("not yet implemented lines")
+
+        self.f.write("\ntable[row sep=crcr]{\n")
+        for key, value in series.items():
+            self.f.write(str(key) + "\t" + str(value) + "\\\\ \n" )
+        self.f.write("};\n")
+        self.f.write("\\addlegendentry{" + name + "}\n")
+    
+    def finishFile(self):
+        self.f.write(self.style["end_lines"])
+
+
 if __name__ == "__main__":
     cfg = TikzPlot("hey.tikz")
+    args=["grid = major"]
+    cfg.setConfiguration(1980, 2014, 0, 10000, False, True)
+    cfg.addSeries({1980 : 900, 2014 : 9000}, "hello")
+    cfg.finishFile()
