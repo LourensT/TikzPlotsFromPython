fad84504962a0a1d885ae2467f204af0869f61df
LourensT <touwenlourens@gmail.com>
2021-08-28 17:46:01 +0200

    restructure

--- GenerateTikz.py
+++ GenerateTikz.py
@@ -1,62 +1,17 @@
 import atexit
 from typing import Dict, List
 
+from style import style
 
-class TikzPlot:
+
+class GenerateTikz:
 
     counter = 0
 
-    style = {
-        "scatter_styling" :
-        [
-            "[color=mycolor1, line width = 2pt, mark size=2pt, mark=diamond*, only marks, mark options={solid, mycolor1}]",
-            "[color=mycolor2, line width = 2pt, mark size=1.5pt, mark=*, only marks, mark options={solid, mycolor2}]",
-            "[color=mycolor3, line width = 2pt, mark size=1.5pt, mark=square*, only marks, mark options={solid, mycolor3}]", 
-            "[color=mycolor4, line width = 2pt, mark size=1.5pt, mark=triangle*, only marks, mark options={solid, mycolor4}]"
-        ],
-        "line_styling":
-        [
-            "[color=mycolor1,line width = 2pt]",
-            "[color=mycolor2,line width = 2pt]",
-            "[color=mycolor3,line width = 2pt]",
-            "[color=mycolor4,line width = 2pt]",
-            "[color=mycolor5,line width = 2pt]",
-            "[color=mycolor1,line width = 2pt]",
-            "[color=mycolor2,line width = 2pt]",
-            "[color=mycolor3,line width = 2pt]"
-        ],
-
-        "start_lines" : 
-'''\\tikzstyle{every node}=[font=\\normalsize]
-
-\\begin{tikzpicture}
-    \\begin{axis}[%
-'''
-        ,
-
-        "constant_configuration":
-'''width=0.951\\fwidth,
-height=\\fheight,
-at={(0\\fwidth,0\\fheight)},
-scale only axis,
-xminorticks=true,
-yminorticks=true,
-axis x line*=bottom,
-axis y line*=left,
-x tick label style = {/pgf/number format/.cd, scaled x ticks = false, set thousands separator={}, fixed},
-axis background/.style={fill=white},
-legend style={legend pos=north west, legend cell align=left, align=left, draw=white!15!black}'''
-        ,
-
-        "end_lines" : 
-'''
-	\end{axis}
-\end{tikzpicture}
-'''
-    }
-
-    def __init__(self, fp):
+    def __init__(self, fp, include_colors = False):
         assert '.tikz' in fp, "should be .tikz file"
+
+        self.style = style
 
         self.f = open(fp, 'w')
         
@@ -64,6 +19,9 @@ legend style={legend pos=north west, legend cell align=left, align=left, draw=wh
         atexit.register(self.f.close)
 
         self.f.write(self.style["start_lines"])
+
+        if include_colors:
+            self.f.write(self.style["colors"])
 
     def setConfiguration(self,  xmin, xmax, 
                                 ymin, ymax, 
@@ -110,7 +68,7 @@ legend style={legend pos=north west, legend cell align=left, align=left, draw=wh
 
 
 if __name__ == "__main__":
-    cfg = TikzPlot("hey.tikz")
+    cfg = GenerateTikz("hey.tikz")
     #args=["grid = major"]
     cfg.setConfiguration(1980, 2014, 0, 10000, False, True)
     cfg.addSeries({1980 : 900, 2014 : 9000}, "hello")
---
+++ style.py
@@ -0,0 +1,59 @@
+style = {
+    "colors" : 
+
+'''	\\definecolor{mycolor1}{rgb}{0.00000,0.80000,0.80000}
+	\\definecolor{mycolor2}{rgb}{0.00000,0.50000,0.50000}
+	\\definecolor{mycolor3}{rgb}{0.00000,0.30000,0.30000}
+	\\definecolor{mycolor4}{rgb}{0.00000,0.10000,0.10000}
+	\\definecolor{mycolor5}{rgb}{0.00000,1,1}
+''',
+
+    "scatter_styling" :
+[
+    "[color=mycolor1, line width = 2pt, mark size=2pt, mark=diamond*, only marks, mark options={solid, mycolor1}]",
+    "[color=mycolor2, line width = 2pt, mark size=1.5pt, mark=*, only marks, mark options={solid, mycolor2}]",
+    "[color=mycolor3, line width = 2pt, mark size=1.5pt, mark=square*, only marks, mark options={solid, mycolor3}]", 
+    "[color=mycolor4, line width = 2pt, mark size=1.5pt, mark=triangle*, only marks, mark options={solid, mycolor4}]"
+],
+    "line_styling":
+
+[
+    "[color=mycolor1,line width = 2pt]",
+    "[color=mycolor2,line width = 2pt]",
+    "[color=mycolor3,line width = 2pt]",
+    "[color=mycolor4,line width = 2pt]",
+    "[color=mycolor5,line width = 2pt]",
+    "[color=mycolor1,line width = 2pt]",
+    "[color=mycolor2,line width = 2pt]",
+    "[color=mycolor3,line width = 2pt]"
+],
+
+    "start_lines" : 
+
+'''\\tikzstyle{every node}=[font=\\normalsize]
+
+\\begin{tikzpicture}
+    \\begin{axis}[%
+''',
+
+    "constant_configuration":
+
+'''width=0.951\\fwidth,
+height=\\fheight,
+at={(0\\fwidth,0\\fheight)},
+scale only axis,
+xminorticks=true,
+yminorticks=true,
+axis x line*=bottom,
+axis y line*=left,
+x tick label style = {/pgf/number format/.cd, scaled x ticks = false, set thousands separator={}, fixed},
+axis background/.style={fill=white},
+legend style={legend pos=north west, legend cell align=left, align=left, draw=white!15!black}''',
+
+    "end_lines" : 
+
+'''
+	\end{axis}
+\end{tikzpicture}
+'''
+}
